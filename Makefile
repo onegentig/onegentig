94774dd704b61cb04447425dd5533b8d6935142a
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-08-21 01:22:44 +0200

    refactor: :construction_worker: utilise remote `me.json`, extend codeblock building

--- Makefile
+++ Makefile
@@ -9,11 +9,20 @@ OUT := README.md
 CP := cp -f
 RM := rm -f
 
-LANGS_IMG_LIST := $(RAW_DIR)/.langlist
-LANGS_IMG := $(IMG_DIR)/languages.png
+.PHONY: all img clean
+
+# Scripts #
+
+BADGE_LIST_SRC := $(SRC_DIR)/badge.sh
 CODEBLOCK_SRC := $(SRC_DIR)/codeblock.ts
 
-.PHONY: all img
+# Data files #
+
+ME_JSON_REMOTE := https://onegen.cyou/me.json
+ME_JSON := $(DAT_DIR)/me.json
+PROFILE_YAML := $(DAT_DIR)/profile.yaml
+LANGS_IMG_LIST := $(RAW_DIR)/.langlist
+LANGS_IMG := $(IMG_DIR)/languages.png
 
 ### Colour codes ###
 
@@ -26,18 +35,44 @@ CYAN := \033[96;1m
 
 ###############################################################################
 
-all:
+all: img $(OUT) clean
+
+img: $(LANGS_IMG)
+
+########## README.md building ##########
+
+$(OUT): $(ME_JSON) $(PROFILE_YAML)
 	@command -v deno >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RED)Deno runtime missing!$(RST)"; exit 127; }
-	@echo -e "📑 $(CYAN)Rebuilding $(EMPH)profile.yaml$(RST)"
-	./$(CODEBLOCK_SRC)
 	@echo -e "📑 $(CYAN)Rebuilding $(EMPH)README.md$(RST)"
 	./$(EXE) > $(OUT)
 	@echo -e "✅ $(GREEN)All done!$(RST)"
 
-img: $(LANGS_IMG_LIST)
+clean:
+	@echo -e "🧹 $(CYAN)Cleaning up$(RST)"
+	$(RM) $(ME_JSON) $(PROFILE_YAML) $(LANGS_IMG_LIST)
+
+########## Images building ##########
+
+$(LANGS_IMG): $(LANGS_IMG_LIST)
 	@command -v montage >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RED)montage ($(EMPH)ImageMagick$(RST)$(RED)) command missing!$(RST)"; exit 127; }
 	@command -v convert >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RET)convert ($(EMPH)ImageMagick$(RST)$(RED)) command missing!$(RST)"; exit 127; }
 	@echo -e "📸 $(CYAN)Rebuilding $(EMPH)languages.png$(RST)"
 	montage -mode concatenate -tile x1 -geometry 80x80\+10+0 -background transparent \
 		`cat -s $(LANGS_IMG_LIST) | sed 's#^#$(RAW_DIR)/#'` miff:- \
 		| convert - -trim $(LANGS_IMG)
+
+########## Data fetching and processing ##########
+
+$(ME_JSON):
+	@command -v curl >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RED)curl command missing!$(RST)"; exit 127; }
+	@echo -e "📥 $(CYAN)Fetching $(EMPH)me.json$(RST)"
+	curl -s $(ME_JSON_REMOTE) > $(ME_JSON)
+
+$(PROFILE_YAML): $(ME_JSON)
+	@command -v deno >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RED)Deno runtime missing!$(RST)"; exit 127; }
+	@echo -e "📑 $(CYAN)Rebuilding $(EMPH)profile.yaml$(RST)"
+	./$(CODEBLOCK_SRC) > $(PROFILE_YAML)
+
+$(LANGS_IMG_LIST): $(ME_JSON)
+	@command -v jq >/dev/null 2>&1 || { echo -e >&2 "🚫 $(RED)jq command missing!$(RST)"; exit 127; }
+	$(BADGE_LIST_SRC) > $(LANGS_IMG_LIST)
--- src/codeblock.ts
+++ src/codeblock.ts
@@ -1,23 +1,129 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write
 
-import { parse } from 'https://deno.land/std@0.192.0/yaml/mod.ts';
+/* === Imports === */
+
+import { writeAll } from 'https://deno.land/std@0.198.0/streams/write_all.ts';
+import me from '../data/me.json' assert { type: 'json' };
+
+type StackData = Record<
+     string,
+     Array<{ name: string; badge?: number; overview?: boolean }>
+>;
+
+/* === Utilities === */
+
+/**
+ * Create a padded string for the tech-stack.
+ * @param stack Stack object
+ * @returns String
+ */
+export function createStack(stack: StackData): string {
+     const categ = Object.keys(stack);
+     const categLen = categ.length;
+     const filtStack: StackData = {};
+     const categStrArr = Array<string>(categLen);
+     let categMaxLen = 0;
+     let categTitleMaxLen = 0;
+
+     for (let i = 0; i < categLen; i++) {
+          /* Category title */
+          categStrArr[i] = `Stack.${titleCase(categ[i])}: `;
+
+          /* Adjust required padding */
+          if (categStrArr[i].length > categTitleMaxLen)
+               categTitleMaxLen = categStrArr[i].length;
+
+          /* Category filtering */
+          filtStack[categ[i]] = stack[categ[i]].filter(
+               (lang) => lang.overview !== false
+          );
+
+          /* Category length */
+          if (filtStack[categ[i]].length > categMaxLen)
+               categMaxLen = filtStack[categ[i]].length;
+     }
+
+     /* Apply padding */
+     for (let i = 0; i < categLen; i++)
+          categStrArr[i] = categStrArr[i].padEnd(categTitleMaxLen);
+
+     /* Vertically iterate over languages */
+     for (let i = 0; i < categMaxLen; i++) {
+          const langStrArr = Array<string>(categLen);
+          let langMaxLen = 0;
+
+          /* Add language to array */
+          for (let j = 0; j < categLen; j++) {
+               const lang = filtStack[categ[j]][i];
+               if (!lang) continue;
+               langStrArr[j] = `● ${lang.name} `;
+               /* Adjust required padding */
+               if (langStrArr[j].length > langMaxLen)
+                    langMaxLen = langStrArr[j].length;
+          }
+
+          for (let j = 0; j < categLen; j++) {
+               /* Apply padding */
+               if (!langStrArr[j]) continue;
+               langStrArr[j] = langStrArr[j].padEnd(langMaxLen);
+
+               /* Add to category string */
+               categStrArr[j] += langStrArr[j];
+          }
+     }
+
+     return categStrArr.join('\n');
+}
 
 /**
  * Calculates long-term uptime from a start date
  * in years and months.
  * @param start The start date
+ * @param hbLast Date of last heartbeat (optional)
  * @returns The uptime string
  */
-function calculateUptime(start: string): string {
+export function calculateUptime(start: string, hbLast?: string): string {
      const startDate = new Date(start);
-     const now = new Date();
+     let endDate = new Date();
 
-     const yr = now.getFullYear() - startDate.getFullYear();
-     const mo = now.getMonth() - startDate.getMonth();
+     if (hbLast) {
+          const hbDate = new Date(hbLast);
+          const twoMonthsMs =
+               60 /* d */ *
+               24 /* h */ *
+               60 /* min */ *
+               60 /* s */ *
+               1000; /* ms */
+          if (endDate.getTime() - hbDate.getTime() > twoMonthsMs)
+               endDate = hbDate;
+     }
+
+     let yr = endDate.getFullYear() - startDate.getFullYear();
+     let mo = endDate.getMonth() - startDate.getMonth();
+
+     if (mo < 0) {
+          mo += 12;
+          yr -= 1;
+     }
 
      let str = `${yr} year${yr > 1 ? 's' : ''}`;
      if (mo > 0) str += `, ${mo} month${mo > 1 ? 's' : ''}`;
      return str;
+}
+
+/**
+ * Converts string to title case.
+ * @see https://www.geeksforgeeks.org/convert-string-to-title-case-in-javascript/
+ * @param str String to convert
+ * @returns Title Case String
+ */
+function titleCase(str: string): string {
+     const strArr = str.toLowerCase().split(' ');
+     for (let i = 0; i < strArr.length; i++) {
+          strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1);
+     }
+
+     return strArr.join(' ');
 }
 
 /**
@@ -34,57 +140,34 @@ function lifeVersion(): string {
      return `${mile}.${cent}${dec}.${rem}`;
 }
 
-/**
- * Create a profile.yaml for a codeblock in the README
- * from the profile.json file.
- * @param data The profile.json data
- * @returns The profile.yaml string
- */
-function createProfileYaml(data: any) {
-     let yaml = `Onegen ~ life version ${lifeVersion()}\n`;
-     yaml += '-'.repeat(yaml.length - 1) + '\n';
-     console.error(
-          'codeblock: remote fetch not implemented',
-     );
+/* === Main === */
 
-     yaml += `Name: ${data.Name}\n`;
-     yaml += `Uptime: ${calculateUptime(data.dob as string)}\n`;
-     yaml += `Kernel: ${data.Kernel}\n`;
-     yaml += `IDE: ${data.IDE}\n`;
-     yaml += `Env: ${data.Env}\n\n`;
+if (import.meta.main) {
+     let profile = `Onegen ~ life version ${lifeVersion()}\n`;
+     profile += '-'.repeat(profile.length - 1) + '\n';
 
-     for (const [sName, stack] of Object.entries(data.Stack))
-          yaml += `Stack.${sName}: ${stack}\n`;
+     /* Basic info */
+     profile += `Name: ${me.name}\n`;
+     profile += `Uptime: ${calculateUptime(me.birthday, me.lastCheckIn)}\n`;
+     profile += `Kernel: ${me.kernel}\n`;
+     profile += `IDE: ${me.ide}\n`;
+     profile += `Env: ${me.env}\n\n`;
 
-     yaml += `\nSocials: ${data.Socials.join(', ')}`;
-     return yaml;
-}
+     /* Tech-Stack */
+     profile += createStack(me.stack);
 
-/**
- * Get the old profile.yaml file.
- * @returns The old profile.yaml data
- */
-function getOld() {
-     try {
-          const yaml = Deno.readTextFileSync('./data/profile.yaml').split('\n');
-          yaml.shift();
-          yaml.shift();
-          yaml.pop();
-
-          return parse(yaml.join('\n'));
-     } catch {
-          return undefined;
+     /* Socials */
+     profile += '\n\nSocials: ';
+     const socials = Array<string>();
+     for (let i = 0; i < me.socials.length; i++) {
+          const social = me.socials[i];
+          if (social.shown !== true) continue;
+          socials.push(`${social.tag} (${social.name})`);
      }
+     profile += socials.join(', ');
+
+     /* Export */
+     const profileOut = new TextEncoder().encode(profile);
+     await writeAll(Deno.stdout, profileOut);
+     console.error('codeblock: Finished');
 }
-
-const data = JSON.parse(
-     await Deno.readTextFile('./data/profile.json'),
-) as Record<string, unknown>;
-console.error(
-     `codeblock: JSON parsed, found ${Object.keys(data).length} keys`,
-);
-
-const yaml = createProfileYaml(data);
-const yamlOut = new TextEncoder().encode(yaml);
-await Deno.writeFile('./data/profile.yaml', yamlOut);
-console.error('codeblock: Finished');
